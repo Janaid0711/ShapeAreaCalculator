2bd3f7ca5a16d42f60bb76abe42e85a3b75cb6e2
JanaidShaik <ST10439595@vcconnect.edu.za>
2025-03-13 14:19:33 +0200

    Circle

---
+++ ShapeAreaCalculator/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeAreaCalculator
+{
+    internal class Circle: Shape, ICalculateArea
+    {
+        public double Radius { get; set; }
+        //Constructor
+        public Circle(string name, double radius) : base(name)
+        {
+            Radius = radius;
+        }
+        //Method to calculate area
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+        //Override Display method
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine($"Radius: {Radius}, Area: {CalculateArea()}");
+        }
+    }
+}
