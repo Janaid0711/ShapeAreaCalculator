29f9b84e21d843c8ee6a5d133a3af708856b7579
JanaidShaik <ST10439595@vcconnect.edu.za>
2025-03-13 14:21:45 +0200

    Main

--- ShapeAreaCalculator/Program.cs
+++ ShapeAreaCalculator/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            
+            //Create objects
+            Rectangle rectangle = new Rectangle("Rectangle", 10, 20);
+            rectangle.Display();
+
+            Console.WriteLine(); //Leave a Line
+
+            Circle circle = new Circle("Circle", 10);
+            circle.Display();
         }
     }
 }
