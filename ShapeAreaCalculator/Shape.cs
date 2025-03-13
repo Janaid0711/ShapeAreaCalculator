b23429af190abcc64539ec07814e04fac64c7051
JanaidShaik <ST10439595@vcconnect.edu.za>
2025-03-13 14:11:41 +0200

    Abstract Class Shape Code

---
+++ ShapeAreaCalculator/Shape.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeAreaCalculator
+{
+    public abstract class Shape
+    {
+        public string Name { get; set; }
+
+        //Constructor
+        public Shape(string name)
+        {
+            Name = name;
+        }
+
+        //Create display
+        public virtual void Display()
+        {
+            Console.WriteLine($"Shape: {Name}" );
+        }
+    }
+}
